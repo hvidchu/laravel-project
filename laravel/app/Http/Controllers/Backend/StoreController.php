bba162cebc1100fe146c10404c988ef701607822
rbaiAra <rbai@fontrip.com>
2020-10-08 13:36:06 +0800

    新增model/controller

---
+++ laravel/app/Http/Controllers/Backend/StoreController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Store;
+
+class StoreController extends Controller
+{
+    public function edit(){
+        //
+    }
+
+    public function update(Request $request){
+        //
+    }
+}
